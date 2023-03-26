df420dc7bc3a26b1cfd396ad426e283eb8ebd712
Eshan Wells <eshanwells@gmail.com>
2023-03-27 00:40:34 +0100

    I2C intiial funcs

--- src/Include/I2C.h
+++ src/Include/I2C.h
@@ -8,10 +8,38 @@
  * @brief initialise I2C as a master, 100khz
  * 
  */
-void initI2C(void);
+void initI2C (void);
 
-inline void I2CEnable(void);
+/**
+ * @brief enable module
+ * 
+ */
+inline void I2CEnable (void);
 
-inline void I2CDisable(void);
+/**
+ * @brief disable module
+ * 
+ */
+inline void I2CDisable (void);
+
+//void I2CWrite (uint8_t slaveAddr, uint8_t* reg_addr, uint8_t reg_len, uint8_t* data, uint8_t len);
+
+/**
+ * @brief Barebones transmit
+ * 
+ * @param slaveAddr only supports 7 bit adressing mode
+ * @param data pointer to data, array suggested
+ * @param len length
+ */
+void I2CTransmit (uint8_t slaveAddr, uint8_t* data, uint8_t len);
+
+/**
+ * @brief 
+ * 
+ * @param slaveAddr 
+ * @param data 
+ * @param len 
+ */
+void I2CReceive (uint8_t slaveAddr, uint8_t* data, uint8_t len);
 
 #endif
