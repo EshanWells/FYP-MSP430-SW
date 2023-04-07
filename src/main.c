6bf6a4d3561bb7533ab23218c15183abe1cf0dcc
Eshan Wells <eshanwells@gmail.com>
2023-04-07 02:30:57 +0100

    please

--- src/main.c
+++ src/main.c
@@ -16,6 +16,15 @@ int main(void)
 
 	while(1)
 	{
-	    _nop();
+    if(doTickRoutine())
+    {
+    static volatile uint64_t count = 0;
+    LED_TGLE;
+
+    char messageHolder[128] = {0};
+    sprintf(messageHolder, "Tick: %d | %d %d %d %d %x %d \n", count, 1, 2, 3, 4, who_am_i, 6);
+    uartPrintString(messageHolder, 128);
+    count++;
+    }
 	}
 }
