0f7cb7b416708025548492652eaaf8a068fe9e36
Eshan Wells <eshanwells@gmail.com>
2023-04-07 02:58:48 +0100

    Another try

--- src/main.c
+++ src/main.c
@@ -20,12 +20,12 @@ int main(void)
 	{
     if(doTickRoutine())
     {
-    static volatile uint64_t count = 0;
+    static volatile uint16_t count = 0;
     LED_TGLE;
 
     char messageHolder[128] = {0};
     sprintf(messageHolder, "Tick: %d | %d %d %d %d %x %d \n", count, 1, 2, 3, 4, who_am_i, 6);
-    uartPrintString(messageHolder, 128);
+    uartPrintString(messageHolder, 64);
     count++;
     }
 	}
